8b578d7c08cda8cbe22dffebbbe249e0c82736da
4lDO2 <4lDO2@protonmail.com>
2019-07-28 12:43:58 +1000

    Complete the superblock parsing!

--- betterfs/src/superblock.rs
+++ betterfs/src/superblock.rs
@@ -1,10 +1,11 @@
 use std::{
     ffi::CString,
+    fmt,
     fs::File,
     io::{prelude::*, SeekFrom},
 };
 
-use fs_core::{read_u8, read_u16, read_u32, read_u64, read_uuid};
+use fs_core::{read_u8, read_u16, read_u32, read_u64, read_uuid, write_u8, write_u64};
 
 const SUPERBLOCK_OFFSET: u64 = 65536;
 const CHECKSUM_SIZE: usize = 32;
@@ -47,7 +48,21 @@ pub struct Superblock {
 
     device_properties: DeviceProperties,
     device_label: CString, // TODO: Is this really a C string?
+
+    cache_generation: u8,
+    uuid_tree_generation: u8,
+    metadata_uuid: uuid::Uuid,
+    system_chunk_array: SystemChunkArray,
+    root_backups: [RootBackup; 4],
 }
+pub struct SystemChunkArray([u8; 2048]);
+
+impl fmt::Debug for SystemChunkArray {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_tuple("SystemChunkArray").finish()
+    }
+}
+
 #[derive(Debug)]
 pub struct DeviceProperties {
     id: u64,
@@ -94,7 +109,7 @@ impl Superblock {
         file.read_exact(&mut block).unwrap();
 
         let mut checksum = [0u8; CHECKSUM_SIZE];
-        checksum.copy_from_slice(&block[..32]);
+        checksum.copy_from_slice(&block[..=31]);
 
         let fs_id = read_uuid(&block, 32);
 
@@ -167,11 +182,23 @@ impl Superblock {
         };
 
         let device_label = {
-            let label_bytes = &block[299..554];
+            let label_bytes = &block[299..=554];
             let nul_position = label_bytes.iter().copied().position(|byte| byte == 0).unwrap();
             let label_bytes_to_nul = &label_bytes[..nul_position];
             CString::new(label_bytes_to_nul).unwrap()
         };
+
+        let cache_generation = read_u8(&block, 555);
+        let uuid_tree_generation = read_u8(&block, 556);
+        let metadata_uuid = read_uuid(&block, 557);
+
+        let mut system_chunk_array = [0u8; 2048];
+        system_chunk_array.copy_from_slice(&block[811..=2858]);
+
+        let mut root_backups = [Default::default(); 4];
+        for (index, backup) in root_backups.iter_mut().enumerate() {
+            *backup = RootBackup::from_raw(&block[2859 + index * RootBackup::RAW_SIZE .. 2859 + (index + 1) * RootBackup::RAW_SIZE]);
+        }
 
         Self {
             checksum,
@@ -203,6 +230,97 @@ impl Superblock {
             log_root_level,
             device_properties,
             device_label,
+            cache_generation,
+            uuid_tree_generation,
+            metadata_uuid,
+            system_chunk_array: SystemChunkArray(system_chunk_array),
+            root_backups,
         }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+pub struct RootBackup {
+    tree_root: u64,
+    tree_root_generation: u64,
+    chunk_root: u64,
+    chunk_root_generation: u64,
+    extent_root: u64,
+    extent_root_generation: u64,
+    filesystem_root: u64,
+    filesystem_root_generation: u64,
+    device_root: u64,
+    device_root_generation: u64,
+    checksum_root: u64,
+    checksum_root_generation: u64,
+    total_bytes: u64,
+    bytes_used: u64,
+    device_count: u64,
+    tree_root_level: u8,
+    chunk_root_level: u8,
+    extent_root_level: u8,
+    filesystem_root_level: u8,
+    device_root_level: u8,
+    checksum_root_level: u8,
+}
+
+impl RootBackup {
+    pub const RAW_SIZE: usize = 168;
+
+    pub fn from_raw(bytes: &[u8]) -> Self {
+        assert!(bytes.len() >= Self::RAW_SIZE);
+
+        Self {
+            tree_root: read_u64(bytes, 0),
+            tree_root_generation: read_u64(bytes, 8),
+            chunk_root: read_u64(bytes, 16),
+            chunk_root_generation: read_u64(bytes, 24),
+            extent_root: read_u64(bytes, 32),
+            extent_root_generation: read_u64(bytes, 40),
+            filesystem_root: read_u64(bytes, 48),
+            filesystem_root_generation: read_u64(bytes, 56),
+            device_root: read_u64(bytes, 64),
+            device_root_generation: read_u64(bytes, 72),
+            checksum_root: read_u64(bytes, 80),
+            checksum_root_generation: read_u64(bytes, 88),
+            total_bytes: read_u64(bytes, 96),
+            bytes_used: read_u64(bytes, 104),
+            device_count: read_u64(bytes, 112),
+            // 120..=151 unused
+            tree_root_level: read_u8(bytes, 152),
+            chunk_root_level: read_u8(bytes, 153),
+            extent_root_level: read_u8(bytes, 154),
+            filesystem_root_level: read_u8(bytes, 155),
+            device_root_level: read_u8(bytes, 156),
+            checksum_root_level: read_u8(bytes, 157),
+            // 158..=167 unused
+        }
+    }
+    pub fn to_raw(this: Self, bytes: &mut [u8]) {
+        assert!(bytes.len() >= Self::RAW_SIZE);
+
+        write_u64(bytes, 0, this.tree_root);
+        write_u64(bytes, 8, this.tree_root_generation);
+        write_u64(bytes, 16, this.chunk_root);
+        write_u64(bytes, 24, this.chunk_root_generation);
+        write_u64(bytes, 32, this.extent_root);
+        write_u64(bytes, 40, this.extent_root_generation);
+        write_u64(bytes, 48, this.filesystem_root);
+        write_u64(bytes, 56, this.filesystem_root_generation);
+        write_u64(bytes, 64, this.device_root);
+        write_u64(bytes, 72, this.device_root_generation);
+        write_u64(bytes, 80, this.checksum_root);
+        write_u64(bytes, 88, this.checksum_root_generation);
+        write_u64(bytes, 96, this.total_bytes);
+        write_u64(bytes, 104, this.bytes_used);
+        write_u64(bytes, 112, this.device_count);
+        // 120..=151 unused
+        write_u8(bytes, 152, this.tree_root_level);
+        write_u8(bytes, 153, this.chunk_root_level);
+        write_u8(bytes, 154, this.extent_root_level);
+        write_u8(bytes, 155, this.filesystem_root_level);
+        write_u8(bytes, 156, this.device_root_level);
+        write_u8(bytes, 157, this.checksum_root_level);
+        // 158..=167 unused
     }
 }
