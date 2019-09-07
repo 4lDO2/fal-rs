454c8af613ceebb8344109734f89a231c1a1bf75
4lDO2 <4lDO2@protonmail.com>
2019-09-07 13:26:31 +1000

    Add a WIP redox filesystem frontend.

---
+++ extfs-redox/src/main.rs
@@ -0,0 +1,30 @@
+use std::{
+    io::prelude::*,
+    fs::File,
+    mem,
+};
+
+use syscall::{data::Packet, Scheme};
+
+struct ExtfsScheme {
+}
+
+impl Scheme for ExtfsScheme {
+    
+}
+
+
+fn main() {
+    let mut socket = File::create("ext2:").expect("Failed to create scheme.");
+
+    let scheme = ExtfsScheme {};
+
+    loop {
+        let mut packet = Packet::default();
+
+        while socket.read(&mut packet).unwrap() == mem::size_of::<Packet>() {
+            scheme.handle(&mut packet);
+            socket.write(&packet).unwrap();
+        }
+    }
+}
