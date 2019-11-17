8dda399fbf9d4fd01e7c1b7c8a57d51843112ae5
4lDO2 <4lDO2@protonmail.com>
2019-11-17 11:56:05 +1100

    Add ext3 and ext4 images as well.

--- test-suite/all.sh
+++ test-suite/all.sh
@@ -4,3 +4,9 @@ mkdir prefix
 if ! sh test-suite/test-fuse-readonly.sh ext2 prefix; then
     exit 1
 fi
+if ! sh test-suite/test-fuse-readonly.sh ext3 prefix; then
+    exit 1
+fi
+if ! sh test-suite/test-fuse-readonly.sh ext4 prefix; then
+    exit 1
+fi
